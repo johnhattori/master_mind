d91fb7360ca9e8673b56c8cc86b4837010a30aa9
john hattori <johnmhattori@yahoo.com>
2014-03-17 22:43:08 -0700

    draw board and play a new game

--- main.py
+++ main.py
@@ -7,6 +7,18 @@ GUESS_SIZE = 4
 NUM_COLORS = 6
 MAX_GUESSES = 12
 
+NUM_COLOR_MAP = {0: 'Y', 
+                 1: 'B', 
+                 2: 'W', 
+                 3: 'P', 
+                 4: 'G', 
+                 5: 'R'}
+assert(len(NUM_COLOR_MAP) <= NUM_COLORS)
+
+COLOR_NUM_MAP = { color: num for (num, color) in NUM_COLOR_MAP.items()}
+
+PEGS = ['-', 'w', 'b']
+
 def generate_target(guess_size=4, num_color=6):
     target = []
     for guess in range(guess_size):
@@ -32,26 +44,34 @@ def score_guess(guess, target):
     score.sort(reverse=True)
     return score
 
+def draw_board(board):
+    """ draws board that's a list of tuples containing guess and score.
+        ex. board = [(guess, score), (guess, score)] 
+            board = [([0, 0, 2, 5], [2,2,1,0])]
+
+             r r b y | b b w - 
+    """
+    to_print = ""
+    for guess, score in board:
+        guess_string = ' '.join([NUM_COLOR_MAP[i] for i in guess])
+        score_string = ' '.join([PEGS[i] for i in score])
+        to_print += guess_string +  '  |  ' +  score_string + '\n'    
+    
+    print(to_print)
+    return to_print
+
 if __name__ == '__main__':
-    target = generate_target(GUESS_SIZE, NUM_COLORS)
-    guess = generate_target(GUESS_SIZE, NUM_COLORS)
-    score = score_guess(guess, target)
-    print("{0}\n{1}\n{2}\n".format(target, guess, score))
+    t = generate_target()
+    b = []
+    for i in range(7):
+        g = generate_target()
+        s = score_guess(t, g)
+        b.append((g,s))
     
-    target = ['y', 'b', 'y', 'b']
-    guess = ['y', 'y', 'y', 'b']
-    score = score_guess(guess, target)
-    print("{0}\n{1}\n{2}\n".format(target, guess, score))
-
-
-    target = ['r', 'b', 'y', 'b']
-    guess = ['y', 'y', 'y', 'b']
-    score = score_guess(guess, target)
-    print("%s\n%s\n%s\n" % (target, guess, score))
-
-    target = ['r', 'y', 't', 'b']
-    guess = ['y', 'y', 't', 't']
-    score = score_guess(guess, target)
-    print("%s\n%s\n%s\n" % (target, guess, score))
-    
+    import pprint
+    pprint.pprint(b)
+    print('')
+    draw_board(b)
+    input()
+    draw_board([(t,[2,2,2,2])])
 
